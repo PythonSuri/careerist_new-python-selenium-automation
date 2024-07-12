2e3d0b96b169ca7d6876edde21f898ce6cf832e5
Surayya Azizzade <surayya@Surayyas-Air.attlocal.net>
2024-07-11 19:20:21 -0500

    index script

--- features/steps/target_search_steps.py
+++ features/steps/target_search_steps.py
@@ -2,9 +2,11 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
+
 @given('Open target main page')
 def open_target(context):
     context.driver.get('https://www.target.com/')
+
 
 @when('Search for product')
 def open_target(context):
@@ -15,6 +17,7 @@ def open_target(context):
     # wait for the page with search results to load
     sleep(4)
 
+
 @then('Verify search worked')
 def verify_search_results(context):
     expected_text = 'coffee'
@@ -22,10 +25,12 @@ def verify_search_results(context):
     assert expected_text in actual_text, f'Expected text {expected_text} is not in actual text {actual_text}'
     sleep(4)
 
+
 @when('Click on Cart icon')
 def click_cart_icon(context):
     context.driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon']").click()
     sleep(4)
+
 
 @then('Verify Your cart is empty message is shown')
 def verify_cart_message(context):
@@ -34,15 +39,18 @@ def verify_cart_message(context):
     assert expected_text in actual_text, f'Expected text {expected_text} is not in actual text {actual_text}'
     sleep(4)
 
+
 @when('Click on Sign In')
 def click_sign_in(context):
     context.driver.find_element(By.XPATH, "//span[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']").click()
     sleep(4)
 
+
 @when('From right side navigation menu click Sign In')
 def click_side_nav(context):
     context.driver.find_element(By.XPATH, "//a[@data-test='accountNav-signIn']").click()
     sleep(4)
+
 
 @then('Verify Sign In form opened')
 def verify_signin_form(context):
